8f0a40067f7d1e41e328e99662c21bf78556c501
Elliot <hack00mind@gmail.com>
2025-09-12 16:53:20 +0800

    chore: replace eslint with oxlint

--- eslint.config.js
+++
@@ -1,4 +0,0 @@
-import baseConfig from "@docube/eslint-config/base.js"
-
-/** @type {import("eslint").Linter.Config[]} */
-export default [...baseConfig]
--- packages/markdown/eslint.config.js
+++
@@ -1,3 +0,0 @@
-import baseConfig from "@docube/eslint-config/base.js";
-
-export default [...baseConfig];
